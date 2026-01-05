be4eb39b349f78c8cc849c4a37fc1f975d0deee0
LeoHong123456 <zh4987758>
2026-01-06 02:25:34 +0800

    1

--- Application/Common/Conf/sms.php
+++ Application/Common/Conf/sms.php
@@ -5,7 +5,7 @@ return array(
     'SMS_API_URL' => 'https://api.onbuka.com/v3/sendSms',
     'SMS_API_KEY' => 'JN85gvcv',
     'SMS_API_SECRET' => 'KJ1kPhA7',
-    'SMS_APP_ID' => 'cs_2fii8n',
+    'SMS_APP_ID' => '46BShokC',
 
     // 短信模板
     'SMS_TEMPLATE' => '您的验证码是：{code}，5分钟内有效，请勿泄露给他人。',
